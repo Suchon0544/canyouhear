44edf26366093fadff4cca09cf490c1e099345a5
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2019-12-01 20:57:16 +0700

    Update Really 1.go
    edit t.Error i went wrong abt double quote

--- Week 3-2/Really 1.go
+++ Week 3-2/Really 1.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestAverage(t *testing.T){
 	v:=Average(3,5)
-	if v!=4{
-		t.Error("Average of 3,5 is 4,not, v.")
+	if v != 4 {
+		t.Error("Average of 3,5 is 4,not", v)
 	}
 }
