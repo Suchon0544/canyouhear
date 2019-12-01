244f1dcd0883a358a02eaf4d54f13b406e40dac2
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2019-12-01 22:29:35 +0700

    Update and rename myTruelyText_test.go to myTruelyText_test_error version.go

--- Week 3-2/myTruelyText_test_error version.go
+++ Week 3-2/myTruelyText_test_error version.go
@@ -1,14 +1,10 @@
 package myTruelyText
 
-import 
-	"fmt"
-	"testing"
+import	"testing"
 
 func TestShowText(t *testing.T){
 	total := ShowText(10,5)
-	if total = 15{
-		fmt.Print("Finally")
-	}
+	
 	if total != 15 {
 		t.Error(total, "is not over")
 	}
