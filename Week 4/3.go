49bac23c836c6eaf956e340c5788d3b6576c9aed
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2019-12-07 22:35:07 +0700

    Add files via upload

---
+++ Week 4/3.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	count := [5]string{"first" , "second" , "third" , "forth" , "fifth"}
+	fmt.Println(count)
+	Length := len(count)
+	fmt.Println(Length)
+	fmt.Println(count[1])
+}
