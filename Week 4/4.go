b2b0bbe022192809e96c49a04d8703ee144065be
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2019-12-07 22:46:33 +0700

    Update 4.go

--- Week 4/4.go
+++ Week 4/4.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main(){
-	counts := [2][2]string{{"first" , "second"},{"Eins","Zwei"}}
+	counts := [2][2]string{{"first" , "second"},{"Eins","Zwei"},{"Hana","Tul"}}
 	fmt.Println(counts)
 }
