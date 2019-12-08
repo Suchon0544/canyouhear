cd37fe6c7d1fca1b3aab54f986c5201acfc38065
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2019-12-08 21:48:40 +0700

    Update 5.go

--- Week 4/5.go
+++ Week 4/5.go
@@ -3,14 +3,7 @@ package main
 import "fmt"
 
 func main(){
-	counts := [2][2][2]string
-	{
-		{"first" , "second"} ,
-		{"Eins" , "Zwei"},
-	} , 
-	{
-		{"Hana" , "Tul"} ,
-		{"Ichi" , "Ni"},
-	}
-	fmt.Println(counts[1][0][0])
+	counts := [2][2][2]string{{{"Fisrt","Second"} , {"Eins","Zwei"}} , {{"Hana","Tul"},{"Ichi","Ni"}}}
+	fmt.Println(counts)
+	fmt.Println(counts[1][1][1])
 }
