54b12e361c6e308d94fd5286c947508765160ed8
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2019-12-08 22:16:34 +0700

    Update 6.go

--- Week 4/6.go
+++ Week 4/6.go
@@ -5,6 +5,9 @@ import "fmt"
 func main(){
 	orders := [] string {"Eins","Zwei","Drei","Viert"}
 	fmt.Println(orders[3:])
+	orders = append(orders ,"Wu","Liu","Qi","Ba")
+	fmt.Println(orders)
 }
 
 //slice by order not index [count from left and slice : count from right and slice]
+//append(var name that you want to add sth , sth that you wanna add to)
