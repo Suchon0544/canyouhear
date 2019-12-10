b984dafd1235e1fa28e79b049626521baf642e35
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2019-12-10 22:16:57 +0700

    Update 1.go

--- Week 5/1.go
+++ Week 5/1.go
@@ -9,26 +9,30 @@ func call_you_name(name string) func(string) string {
 }
 
 func main() {
-	fmt.Println("enter be like name time(hours)")
+	fmt.Println("enter be like name time")
 	fmt.Print("Plaese enter your name and what's time = ")
 	var text string
-	var time int
+	var time float32
 	fmt.Scan(&text, &time)
 	if time >= 18 {
 		call_my_name := call_you_name(text)
 		fmt.Println(call_my_name(" Good Night"))
+		fmt.Print("It's time to go to bed")
 	} else if time >= 15 {
 		call_my_name := call_you_name(text)
 		fmt.Println(call_my_name(" Good Evening"))
+		fmt.Print("Come Back Home!")
 	} else if time >= 12 {
 		call_my_name := call_you_name(text)
 		fmt.Println(call_my_name(" Good Afternoon"))
+		fmt.Print("Have you had lunch, Right?")
 	} else if time >= 6 {
 		call_my_name := call_you_name(text)
 		fmt.Println(call_my_name(" Good Morning"))
+		fmt.Print("Have a good day! ,sunrise have come!")
 	} else if time >= 1 {
 		call_my_name := call_you_name(text)
 		fmt.Println(call_my_name(" Good Night"))
+		fmt.Print("It's time to go to bed")
 	}
 }
-//how lonely people do when they're alone....
