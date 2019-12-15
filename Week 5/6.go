dd96bcf11978a1a6e4bbaa2fee17368b425acc0c
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2019-12-15 20:01:44 +0700

    Update 6.go

--- Week 5/6.go
+++ Week 5/6.go
@@ -6,7 +6,15 @@ func tried() {
 	fmt.Println("Vitamins")
 }
 
+func tolive(){
+	fmt.Println("Extension of you")
+}
+
 func main() {
 	defer tried()
+	defer tolive()
 	fmt.Println("With a billoin worldful of <3")
 }
+
+// defer == defered func will active before running func
+// order(s) upside-down
