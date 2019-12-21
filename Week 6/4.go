9e2fd18667b705ce87a28db1ceac94dc4e544174
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2019-12-21 22:30:32 +0700

    Update 4.go

--- Week 6/4.go
+++ Week 6/4.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+func iNeedHealing() {
+	inh := recover()
+	fmt.Print(inh)
+}
+
 func main() {
+	defer iNeedHealing()
 	panic("You didn't have any map.")
-	defer healing := recover()
 	var name map[int]string
 	name[1] = "Outro : Wings"
 	fmt.Println(name)
