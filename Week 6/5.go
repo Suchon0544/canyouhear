ea206635c90020c4533d6630abd439fc510d9eb3
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2019-12-21 22:50:37 +0700

    Update 5.go

--- Week 6/5.go
+++ Week 6/5.go
@@ -6,7 +6,16 @@ func devide(a, b int) int {
 	return a / b
 }
 
+func helpMe() {
+	helpYou := recover()
+	if helpYou == 0 {
+		fmt.Println("You result should not be zero")
+		main()
+	}
+}
+
 func main() {
+	defer helpMe()
 	c := devide(15, 32)
 	fmt.Println(c)
 }
