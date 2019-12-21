102fcad28a4ae3e018cd96f003d25fa37348a3dd
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2019-12-21 22:23:38 +0700

    Update 3.go

--- Week 6/3.go
+++ Week 6/3.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	panic["You didn't have any map."]
+	panic("You didn't have any map.")
 	var name map[int]string
 	name[1] = "Outro : Wings"
 	fmt.Println(name)
