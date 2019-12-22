f5b1d97af52cba3dc632dc99baa9d1f814bd8e59
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2019-12-22 22:52:56 +0700

    Update 9.go

--- Week 6/9.go
+++ Week 6/9.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func main() {
 	var a int
-	fmt.Scan(&int)
+	fmt.Scan(&a)
 	if a*15 < 100 {
 		fmt.Println("Not Today")
-	}else if a*20 == 60 {
+	} else if a*20 == 60 {
 		fmt.Println("Today we will survived")
-	}else if a*10 > 100 {
+	} else if a*10 > 100 {
 		fmt.Println("Today we fight")
 	}
 }
