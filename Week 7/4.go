e50a97d6acee1ad30271b320ef770449e699a0c2
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2019-12-29 20:09:14 +0700

    Add files via upload

---
+++ Week 7/4.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Album struct {
+	name  string
+	track string
+}
+
+func (alb Album) listen(){
+	fmt.Println("Have you ever hear this ," alb.name)
+}
+
+type music struct {
+	address string
+	alb		Album
+}
+
+func main() {
+	first := Album{name : "Grimoire of Crimson"}
+	mus := music{alb: first}
+	mus.alb.listen()
+}
