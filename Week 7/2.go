ebe0598e41d25f116ed944092ed92553ca55f98b
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2019-12-29 19:24:01 +0700

    Update 2.go

--- Week 7/2.go
+++ Week 7/2.go
@@ -3,19 +3,18 @@ package main
 import "fmt"
 
 type Album struct {
-	name     string
-	track    int
+	name  string
+	track int
 }
 
 func Nexttrack(name string) *Album {
 	GoC := new(Album)
 	GoC.name = "Grimoire of Crimson"
-	GoC.composer = "Team Grimoire"
 	GoC.track = 1
+	return &GoC
 }
 
 func main() {
-	fmt.Println("Music name :", GoC.name)
-	fmt.Println("Composer :", GoC.composer)
-	fmt.Println("Track :", GoC.track)
+	fmt.Println(Album{"Music name :", GoC.name})
+	fmt.Println(Album{"Track :", GoC.track})
 }
