17ab96e60ac63608ba3a4c08563d7d2633236501
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2020-02-07 20:57:22 +0700

    Update 2.go

--- REAL Week 12/2/2.go
+++ REAL Week 12/2/2.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-func deposit() {
-	var dep float64
-	inr := 2.125 / 100
+func deposit(dep float64) float64 {
+	fmt.Print("Plaese enter your deposit = ")
 	fmt.Scan(&dep)
+	inr := 2.125 / 100
 	inter := dep * inr
 	return inter
 }
 
 func main() {
-	de := deposit
-	fmt.Print(de)
+	de := deposit(1)
+	fmt.Print(fmt.Sprintf("Your interest = %v Bath", de))
 }
