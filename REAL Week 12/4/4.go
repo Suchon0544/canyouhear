063b4bd98da1dbc47ac406da71909ff4389e19fc
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2020-02-09 23:08:58 +0700

    Add files via upload

---
+++ REAL Week 12/4/4.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Country struct{
+	name string
+	lang string
+	capitalcity string
+}
+
+func correcting(co *Country){
+	co.lang = "British English"
+}
+
+func main(){
+	var country := Country{
+		name : "United Kingdom of Great Britain and Northern Ireland",
+		lang : "English",
+		capitalcity : "London",
+	}
+
+	correcting(&country)
+
+	fmt.Println(country)
+}
