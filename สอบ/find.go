3a491961a790701e13ffd58bc6f3e597981e609e
ichiyama447 <57866286+ichiyama447@users.noreply.github.com>
2020-02-22 19:26:48 +0700

    Update find.go

--- สอบ/find.go
+++ สอบ/find.go
@@ -4,8 +4,18 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
 	"strings"
+	"sync"
+	"time"
 )
+
+type MyFile struct {
+	Path    string
+	Size    int64
+	Name    string
+	ModTime time.Time
+}
 
 func getDrives() (r []string) {
 	fmt.Print("Directory : ")
@@ -20,30 +30,86 @@ func getDrives() (r []string) {
 	return
 }
 
-func findFileFromExtension(extension []string, dir string, files *[]string) {
+func FindFileFromExtension(extension []string, dir string, files *[]MyFile, wg *sync.WaitGroup) {
+	defer wg.Done()
 	fs, err := ioutil.ReadDir(dir)
-	if err == nil {
-		for _, f := range fs {
-			for _, ex := range extension {
-				if strings.HasSuffix(f.Name(), ex) {
-					*files = append(*files, f.Name())
-				}
-			}
+	if err != nil {
+		return
+	}
+	for _, f := range fs {
+		var subWg sync.WaitGroup
+		subWg.Add(1)
+		if f.IsDir() {
+			path := dir + "/" + f.Name()
+			go FindFileFromExtension(extension, path, files, &subWg)
 
-			if f.IsDir() {
-				path := dir + "/" + f.Name()
-				findFileFromExtension(extension, path, files)
-			}
+		} else {
+			ProcessingExtension(dir, f, extension, files, &subWg)
 		}
+		subWg.Wait()
+	}
+}
+
+
+func ProcessingExtension(dir string, f os.FileInfo, extension []string, files *[]MyFile, wg *sync.WaitGroup) {
+	defer wg.Done()
+	filename := f.Name()
+	index := strings.LastIndex(filename, ".")
+	if index < 0 {
+		return
+	}
+	index = index + 1
+	size := len(filename)
+	ext := filename[index:size]
+	_, ok := extension[ext]
+	if ok {
+		var mf MyFile
+		mf.Path = dir + "/" + f.Name()
+		mf.Size = f.Size()
+		mf.Name = f.Name()
+		mf.ModTime = f.ModTime()
+		*files = append(*files, mf)
+	}
+}
+
+func output(Path, Size []string) {
+	file, err := os.Create("Output.txt")
+	defer file.Close()
+	if err != nil {
+		return
+	}
+	for i := range Path {
+		file.WriteString(fmt.Sprintf("%v\t%v\r\n", Size[i], Path[i]))
 	}
 }
 
 func main() {
+	var wg sync.WaitGroup
+	t1 := time.Now()
+	myfiles := []MyFile{}
+	extensionMap := make(map[string]string)
+	/*extensionMap["jpg"] = ".jpg"*/
+	extensionMap["txt"] = ".txt"
 	drives := getDrives()
-	files := []string{}
-	for _, d := range drives {
-		findFileFromExtension([]string{}, d, &files)
+	wg.Add(len(drives))
+	for _, drive := range drives {
+
+		go FindFileFromExtension(extensionMap, drive, &myfiles, &wg)
+
 	}
-	fmt.Println(len(files))
-	fmt.Println(drives)
+	wg.Wait()
+
+	var pathFiles, sizeFiles []string
+	for _, pathToFiles := range myfiles {
+		pathFiles = append(pathFiles, pathToFiles.Path)
+		sizeFiles = append(sizeFiles, strconv.Itoa(int(pathToFiles.Size)))
+	}
+
+	t2 := time.Now()
+	diftime := t2.Sub(t1)
+	fmt.Println("total files = ", len(myfiles))
+	output(pathFiles, sizeFiles)
+	fmt.Println(pathFiles)
+	fmt.Println("total time = ", diftime)
+
 }
